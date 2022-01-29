6d9e3a87a74dcd2b1500d2c8ce5aaa11d040b107
Edward <wardvisual@gmail.com>
2022-01-29 14:00:01 +0800

    Remove: Logger

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,12 +1,7 @@
 const login = (request, response, next) => {
-    const { username, password } = request.body;
-
-    console.log({
-        username,
-        password,
-    });
-
-    response.json({ token: "432lkjd,asljsalkjlkd-" });
+    try {} catch (err) {
+        console.log(err);
+    }
 };
 
 module.exports = { login };
